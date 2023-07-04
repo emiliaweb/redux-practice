b56dc01b282f6ea55aa2a1ca9a71e316042dae8e
Emilia <81705094+emiliaweb@users.noreply.github.com>
2023-07-04 15:51:53 +0300

    Use redux toolkit

--- src/reducers/heroes.js
+++ src/reducers/heroes.js
@@ -1,44 +1,71 @@
+import { createReducer } from "@reduxjs/toolkit"
+
+import { 
+    heroesFetched,
+    heroesFetching,
+    heroesFetchingError,
+    heroesDeleted,
+    heroesPosted
+} from "../actions"
+
 const initialState = {
     heroes: [],
     heroesLoadingStatus: 'idle'
 }
 
-const heroes = (state = initialState, action) => {
-    switch (action.type) {
-        case 'HEROES_FETCHING':
-            return {
-                ...state,
-                heroesLoadingStatus: 'loading'
-            }
-        case 'HEROES_FETCHED':
-            return {
-                ...state,
-                heroes: action.payload,
-                heroesLoadingStatus: 'idle'
-            }
-        case 'HEROES_POSTED':
-            return {
-                ...state,
-                heroes: [...state.heroes, action.payload],
-                heroesLoadingStatus: 'idle'
-            }
-        case 'HEROES_FETCHING_ERROR':
-            return {
-                ...state,
-                heroesLoadingStatus: 'error'
-            }
-        case 'HEROES_DELETED': 
-            return {
-                ...state, 
-                heroes: state.heroes.filter((hero) => hero.id !== action.payload)
-            }
-        case 'SET_ACTIVE_FILTER': 
-            return {
-                ...state, 
-                activeFilter: action.payload
-            }
-        default: return state
-    }
-}
+const heroes = createReducer(initialState, builder => {
+    builder
+        .addCase(heroesFetching, state => {
+            state.heroesLoadingStatus = 'loading'
+        })
+        .addCase(heroesFetched, (state, action) => {
+            state.heroes = action.payload;
+            state.heroesLoadingStatus = 'idle';
+        })
+        .addCase(heroesFetchingError, state => {
+            state.heroesLoadingStatus = 'error';
+        })
+        .addCase(heroesPosted, (state, action) => {
+            state.heroes.push(action.payload);
+            state.heroesLoadingStatus = 'idle';
+        })
+        .addCase(heroesDeleted, (state, action) => {
+            state.heroes = state.heroes.filter((hero) => hero.id !== action.payload);
+        })
+        .addDefaultCase(() => {})
+})
+
+// const heroes = (state = initialState, action) => {
+//     switch (action.type) {
+//         case 'HEROES_FETCHING':
+//             return {
+//                 ...state,
+//                 heroesLoadingStatus: 'loading'
+//             }
+//         case 'HEROES_FETCHED':
+//             return {
+//                 ...state,
+//                 heroes: action.payload,
+//                 heroesLoadingStatus: 'idle'
+//             }
+//         case 'HEROES_POSTED':
+//             return {
+//                 ...state,
+//                 heroes: [...state.heroes, action.payload],
+//                 heroesLoadingStatus: 'idle'
+//             }
+//         case 'HEROES_FETCHING_ERROR':
+//             return {
+//                 ...state,
+//                 heroesLoadingStatus: 'error'
+//             }
+//         case 'HEROES_DELETED': 
+//             return {
+//                 ...state, 
+//                 heroes: state.heroes.filter((hero) => hero.id !== action.payload)
+//             }
+//         default: return state
+//     }
+// }
 
 export default heroes;
